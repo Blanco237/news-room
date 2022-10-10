4b6fc107c105615b4b6916d66527cb25bf477942
Blanco237 <asongrandy9@gmail.com>
2022-10-10 12:00:09 +0100

    Completed keyword extractor

---
+++ server/utils/keywords.js
@@ -0,0 +1,19 @@
+//how to extract keywords from string in javascript?
+
+const keywordExtractor = (text) => {
+    const commonWords = ['i','a','about','an','and','are','as','at','be','by','com','de','en','for','from','how','in','is','it','la','of','on','or','that','the','this','to','was','what','when','where','who','will','with','und','the','www'];
+
+    // replace unnesessary chararacters. leave only chars, numbers and space
+    const workingText = text.toLowerCase().replace(/[^\w\d ]/g, '');
+
+    // convert to array and remove all common words
+    const result = workingText.split(' ').filter((word) => {
+        return commonWords.indexOf(word) === -1;
+    });
+
+    const uniqueResult = Array.from(new Set(result));
+
+    return uniqueResult;
+}
+
+module.exports = keywordExtractor;
