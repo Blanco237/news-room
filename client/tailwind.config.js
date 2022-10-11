23f1c285ab387c932260e8e49433198586f22238
Blanco237 <asongrandy9@gmail.com>
2022-10-11 17:02:40 +0100

    App Config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary" : "#00bfa5",
+        "secondary" : "#d4f4f0",
+        "gray" : "#f5f5f5"
+      }
+    },
   },
   plugins: [],
 }
