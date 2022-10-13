9ae2c1ee972fca416ebde357d86e4a2dc1ef4b5c
Blanco237 <asongrandy9@gmail.com>
2022-10-13 16:35:55 +0100

    Completed Full Title Card

---
+++ client/src/components/home/CardFullTitle/CardFullTitle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CardFullTitle = ({ img, title, width }) => {
+  return (
+    <div className={`${width} overflow-hidden cursor-pointer`}>
+      <img src={img} alt="News Story" className="w-full aspect-square z-0 object-cover hover:scale-125 transition-all duration-500" />
+      <div className="bg-white md:text-1xl text-lg w-full px-2 py-3 font-bold z-10 text-dark text-left isolate ">
+        <h1 className="link">{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CardFullTitle;
