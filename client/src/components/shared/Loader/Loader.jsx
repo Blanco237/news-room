fced81d329b141a647a3dba630c6e1950b0adc2d
Blanco237 <asongrandy9@gmail.com>
2022-10-14 19:40:25 +0100

    Loader adjustment

--- client/src/components/shared/Loader/Loader.jsx
+++ client/src/components/shared/Loader/Loader.jsx
@@ -6,7 +6,7 @@ import styles from './load.module.css'
 const Loader = ({ height }) => {
   return (
     <section className={`${height} w-full grid place-items-center`}>
-        <div className={`relative aspect-square w-1/12 ${styles.container}`}>
+        <div className={`relative aspect-square md:w-1/12 w-3/12 ${styles.container}`}>
             <img src={loader} alt="Loader Icon" className='w-full' />
         </div>
     </section>
