4a03584d952f1652adb1892a2c56a84a9b2ef295
Blanco237 <asongrandy9@gmail.com>
2022-10-16 23:05:50 +0100

    Completed africa

--- client/src/components/home/Africa/Africa.jsx
+++ client/src/components/home/Africa/Africa.jsx
@@ -1,44 +1,37 @@
-import React from 'react'
-import CardFullTitle from '../CardFullTitle/CardFullTitle';
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
-import africa from './africa.jpg';
-import africa2 from './africa2.jpg';
-import CardOverlay from './../CardOverlay/CardOverlay';
+import CardFullTitle from "../CardFullTitle/CardFullTitle";
+import CardOverlay from "./../CardOverlay/CardOverlay";
+import Loader from "../../shared/Loader/Loader";
+
+import { getData } from "./../../../api/api";
 
 
 const Africa = () => {
-
-    const data= {
-        title: "Hundreds killed in Nigeria floods, more than 1.4 million displaced",
-        img: africa
-    }
-
-    const data2 = {
-        title: "2% of the world's rarest zebras wiped out in Kenya's relentless drought",
-        img: africa2
-    }
+  const { data, isLoading } = useQuery(["africa"], () => getData("/africa"));
 
   return (
-    <section className='flex flex-col px-main py-5 gap-4'>
-        <h1 className='heading'>Happening In Africa</h1>
-        <div className='flex gap-4 md:flex-row flex-col'>
-            <div className='md:w-2/5 w-full grid gap-4 grid-cols-2'>
-                {
-                    [...Array(6)].map(() => {
-                        return <CardFullTitle {...data} width={`w-full`} />
-                    })
-                }
-            </div>
-            <div className='md:w-3/5 w-full grid gap-4 md:grid-cols-3 md:grid-rows-3 grid-cols-2'>
-            {
-                    [...Array(9)].map(() => {
-                        return <CardOverlay {...data2} width={`w-full`} check/>
-                    })
-                }
-            </div>
+    <section className="flex flex-col px-main py-5 gap-4">
+      <h1 className="heading">Happening In Africa</h1>
+      {isLoading ? (
+        <Loader height={`h-[80vh]`} />
+      ) : (
+        <div className="flex gap-4 md:flex-row flex-col">
+          <div className="md:w-2/5 w-full grid gap-4 grid-cols-2">
+            {data.slice(0, 6).map((story) => {
+              return <CardFullTitle {...story} key={story.aid} width={`w-full`} />;
+            })}
+          </div>
+          <div className="md:w-3/5 w-full grid gap-4 md:grid-cols-3 md:grid-rows-3 grid-cols-2">
+            {data.slice(6, 16).map((story) => {
+              return <CardOverlay {...story} key={story.aid} width={`w-full`} check />;
+            })}
+          </div>
         </div>
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default Africa
+export default Africa;
