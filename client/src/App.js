8a0ac38da50a8dfc5dde50b1eebe2e2f40da0b61
Blanco237 <asongrandy9@gmail.com>
2022-10-16 22:16:12 +0100

    Setup main query client

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,18 @@ import Footer from './components/shared/Footer/Footer';
 import Header from './components/shared/Header/Header';
 import Home from './pages/home/Home';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 function App() {
+
+  const client = new QueryClient()
+
   return (
-    <div>
+    <QueryClientProvider client={client}>
       <Header />
       <Home />
       <Footer />
-    </div>
+    </QueryClientProvider>
   );
 }
 
