1720d8fcaaa7a5061fb26f2a53072d83683567af
Blanco237 <asongrandy9@gmail.com>
2022-10-21 00:05:58 +0100

    Created filter function

--- server/utils/filterduplicates.js
+++ server/utils/filterduplicates.js
@@ -1,12 +1,30 @@
 
-const filterduplicates = (arr) => {
-    const filteredArr = arr.filter((value, index, self) =>
-        index === self.findIndex((t) => (
-            t.title === value.title && t.link === value.link
-        ))
-    )
+const oldfilterduplicates = (arr) => {
+    const filteredArr = [...new Map(arr.map(v => [v.link, v])).values()]
 
     return filteredArr;
 }
+
+const filterduplicates = (arr) => {
+    const existing = [];
+    const filtered = arr.map((obj) => {
+        if(existing.includes(obj.title)){
+            return null;
+        }
+        existing.push(obj.title);
+        return obj;
+    }).filter(obj => obj);
+
+    return filtered;
+}
+
+// const objs = [
+//     {title: "Blanco", name: "Randy"},
+//     {title: "Randy", name: "Blanco "},
+//     {title: "Blanco", name: "Asong"},
+//     {title: "Randy", name: "Atem"}
+// ]
+
+// console.log(filterduplicates(objs));
 
 module.exports = filterduplicates;
