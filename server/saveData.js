b9568abb1cc97b068adeb32c7ef197bcd256acc8
Blanco237 <asongrandy9@gmail.com>
2022-10-21 00:05:46 +0100

    Added filter function to datasource

--- server/saveData.js
+++ server/saveData.js
@@ -3,21 +3,33 @@ const { Africa, Full, MostRead, Random, Sport, Tech } = require("./models");
 const filterduplicates = require("./utils/filterduplicates");
 
 const saveData = async ({ BBC, CNN }) => {
+
+    const mostRead = filterduplicates(BBC.mostRead);
+    const randomStories = filterduplicates([...BBC.randomStories, ...CNN.randomStories]);
+    const fullStories = filterduplicates(BBC.fullStories);
+    const sportStories = filterduplicates([...BBC.sportStories, ...CNN.sportStories]);
+    const techStories = filterduplicates(CNN.techStories);
+    const africanStories = filterduplicates(CNN.africanStories);
+
+
     try {
+        console.log(BBC.mostRead);
+        console.log(`Filtered \n \n \n`);
+        console.log();
         try{
-            await MostRead.bulkCreate(filterduplicates(BBC.mostRead));
+            await MostRead.bulkCreate(mostRead);
         }catch(e) {
             console.error(e);
         }
-        await Random.bulkCreate(filterduplicates([...BBC.randomStories, ...CNN.randomStories])) 
-        await Full.bulkCreate(filterduplicates(BBC.fullStories));
-        await Sport.bulkCreate(filterduplicates([...BBC.sportStories, ...CNN.sportStories]));
-        await Tech.bulkCreate(filterduplicates(CNN.techStories));
-        await Africa.bulkCreate(filterduplicates(CNN.africanStories));
+        await Random.bulkCreate(randomStories) 
+        await Full.bulkCreate(fullStories);
+        await Sport.bulkCreate(sportStories);
+        await Tech.bulkCreate(techStories);
+        await Africa.bulkCreate(africanStories);
         console.log(`All data saved`);
     }
     catch(e) {
-        console.error(`Error:: ${e.message}`);
+        console.error(e);
     }
 }
 
